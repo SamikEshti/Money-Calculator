36baf68d4b7b879282345f9079b9354c8ea64869
SamikEshti <ahmedsamik97@gmai.com>
2022-02-17 23:15:44 +0600

    error handle

--- samik.js
+++ samik.js
@@ -1,17 +1,25 @@
-function myFunction(){
+function myCalculator(){
     var rentExpense = Number(rent.value);
     var foodExpense = Number(food.value);
     var clothesExpense = Number(clothes.value);
-    
     var sumIncome = Number(income.value);
-    var sumExpense = rentExpense + foodExpense + clothesExpense;
-    var balance = Number(sumIncome-sumExpense);
+
+    if(sumIncome>=0 && rentExpense>=0 && foodExpense>=0 && clothesExpense>=0){
+        var sumExpense = rentExpense + foodExpense + clothesExpense;
+        var balance = Number(sumIncome-sumExpense);
+
+        document.getElementById("totalExpenses").innerHTML = "Total Expense: " + sumExpense ;
+        document.getElementById("totalBalance").innerHTML = "Balance: " + balance ;
+    }
+    else{
+        document.getElementById("error").innerHTML="Negative values are not acceptable";
+    }
 
     document.getElementById("totalExpenses").innerHTML = "Total Expense: " + sumExpense ;
     document.getElementById("totalBalance").innerHTML = "Balance: " + balance ;
 }
 
-function percentCal(){
+function percentCalculation(){
     var rentExpense = Number(rent.value);
     var foodExpense = Number(food.value);
     var clothesExpense = Number(clothes.value);
